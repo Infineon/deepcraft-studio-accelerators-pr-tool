1f387872cb6aab46d6f6bea18ef6803fde43c7c1
KeidarGad <gad.keidar@imagimob.com>
2025-06-03 15:07:17 +0200

    Allow any .im* file type

--- pr_tool/validation.py
+++ pr_tool/validation.py
@@ -1,11 +1,15 @@
+import fnmatch
 from pathlib import Path
 
 def validate_project_structure(project_name: str, project_path: Path) -> None:
     required_items = {f'{project_name}.improj', 'Data', 'README.md'}
-    allowed_items = {'metadata.json', 'Models', 'PreprocessorTrack', 'Resources', 'Tools', 'Units'}
+    allowed_items = {'*.im*', 'metadata.json', 'Models', 'PreprocessorTrack', 'Resources', 'Tools', 'Units'}
     project_root_items = set(path.name for path in project_path.iterdir())
     if missing_items := required_items - project_root_items:
         raise ValueError(f'Items {missing_items} are missing from project\'s root directory.')
-    if not_allowed_items := project_root_items - required_items - allowed_items:
+    allowed_root_items = []
+    for pattern in required_items | allowed_items:
+        allowed_root_items += fnmatch.filter(project_root_items, pattern)
+    if not_allowed_items := project_root_items - set(allowed_root_items):
         raise ValueError(f'Items {not_allowed_items} are not allowed in project\'s root directory;\n'
                          f'Allowed items are {required_items | allowed_items}')
