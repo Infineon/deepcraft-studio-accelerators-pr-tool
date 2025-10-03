bbb1c076d04f7c13964c2e1d5e7ba41d32f3f37d
KeidarGad <gad.keidar@imagimob.com>
2025-10-03 06:10:40 +0200

    Fix constant fetching during commit

--- pr_tool/pr_tool.py
+++ pr_tool/pr_tool.py
@@ -55,7 +55,7 @@ def fork():
         shutil.rmtree(git_dir, onerror=onerror)
     with TemporaryDirectory() as tmpdir:
         # Clone repo empty
-        git(['clone', '--no-checkout', '--filter=blob:none', '--depth', '1', '--no-single-branch', '--sparse',
+        git(['clone', '--no-checkout', '--depth', '1', '--no-single-branch', '--sparse',
              f'--separate-git-dir={git_dir}', origin_url, tmpdir])
     git(['remote', 'add', '-t', MAIN_BRANCH, 'upstream', BASE_REPO_URL])
     git(['config', 'advice.updateSparsePath', 'false'])
@@ -73,8 +73,8 @@ if gh(['repo', 'sync', f'{user}/{REPO_NAME}', '--force', '--branch', MAIN_BRANCH
     gh(['auth', 'refresh', '--hostname', 'github.com', '-s', 'workflow,delete_repo'])
     gh(['repo', 'delete', f'{user}/{REPO_NAME}', '--yes'])
     fork()
-# Ignore everything (including files in root) but the project
-git(['sparse-checkout', 'set', '--no-cone', '!/*', f'/{project_name}/'])
+# Ignore everything but the project
+git(['sparse-checkout', 'set', project_name])
 
 branch_ref = f'refs/heads/{branch_name}'
 if git(['ls-remote', '--exit-code', '--quiet', 'origin', branch_ref], check=False) == 2:
